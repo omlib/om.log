f38da9c5308a7948c7d9cea38277fb1896b0dc33
tong <tong@disktree.net>
2023-06-13 11:03:41 +0200

    Backup filetransport log files to name.[i] on max size exceed

--- src/om/log/FileTransport.hx
+++ src/om/log/FileTransport.hx
@@ -2,19 +2,24 @@ package om.log;
 
 #if sys
 
+using haxe.io.Path;
+
 class FileTransport extends BaseTransport {
 
     public final file : String;
     public final maxFileSize : Null<Float>; // 1e+9
-    //public final maxLines : Null<Float>;
+    //public final maxHistory : Null<Float>;
+
+    public var fileSize(default,null) : Int;
     public var bytesWritten(default,null) : Int;
 
     var out : sys.io.FileOutput;
 
-    public function new(file:String, ?maxFileSize:Int) {
+    public function new(file="om.log", ?maxFileSize:Int) {
         super();
-        this.file = "om.log";
+        this.file = FileSystem.absolutePath(file);
         this.maxFileSize = maxFileSize;
+        fileSize = FileSystem.stat(file).size;
     }
 
     override function init() {
@@ -23,6 +28,35 @@ class FileTransport extends BaseTransport {
             out = File.append(file);
         } else {
             out = File.write(file);
+        }
+    }
+
+    function output(message:String) {
+        try {
+            out.writeString(message);
+        } catch(e) {
+            trace(e);
+            return;
+        }
+        bytesWritten += message.length;
+        if(maxFileSize != null && (fileSize + bytesWritten) >= maxFileSize) {
+            //trace('max file size not implemented', (fileSize+bytesWritten) );
+            final name = file.withoutDirectory();
+            final reg = new EReg("^"+name+".([0-9]+)$", "");
+            var lastLogFileIndex : Null<Int> = null;
+            for(f in FileSystem.readDirectory(file.directory())) {
+                if(reg.match(f)) {
+                    final i = Std.parseInt(reg.matched(1));
+                    if(lastLogFileIndex == null)
+                        lastLogFileIndex = i;
+                    else if(i > lastLogFileIndex)
+                        lastLogFileIndex = i;
+                }
+            }
+            out.close();
+            final index = (lastLogFileIndex == null) ? 1 : lastLogFileIndex+1;
+            FileSystem.rename(this.file, '${this.file}.$index');
+            out = File.write(this.file);
         }
     }
 
@@ -37,15 +71,6 @@ class FileTransport extends BaseTransport {
 
     //public function changeFilePath
 
-    function output(message:String) {
-        try {
-            out.writeString(message);
-        } catch(e) {
-            trace(e);
-            return;
-        }
-        bytesWritten = message.length;
-    }
 }
 
 #end
