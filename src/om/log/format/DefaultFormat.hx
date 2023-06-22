1d94d8170922c8d32b3cccd57ad852a5203501ae
tong <tong@disktree.net>
2023-06-23 00:54:32 +0200

    Improve default format

--- src/om/log/format/DefaultFormat.hx
+++ src/om/log/format/DefaultFormat.hx
@@ -1,11 +1,17 @@
 package om.log.format;
 
-class DefaultFormat implements Format {
-    public function new() {}
+class DefaultFormat implements om.log.Format {
+
+    public var separator : String;
+
+    public function new(separator=" ") {
+      this.separator = separator;
+    }
+
     public function format(msg: om.log.Message) : String {
-        var date = Date.fromTime(msg.time);
-        var out = '${date.toString()} ${msg.level} ${msg.content}';
-        if(msg.meta != null) out += ' '+Std.string(msg.meta);
-        return out;
+        var time = Std.int(msg.time * 1000) / 1000;
+        var arr = [Std.string(time), Std.string(msg.level), msg.content];
+        if(msg.meta != null) arr.push(Std.string(msg.meta));
+        return arr.join(separator);
     }
 }
